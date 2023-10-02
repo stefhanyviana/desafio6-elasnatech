7915735238cbd66b6b4ecf85ea77f5ce7b2445e8
Stefhany Viana <105253278+stefhanyviana@users.noreply.github.com>
2023-10-02 19:53:25 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var num = parseInt(prompt('Digite um número: '))
+
+if (num % 2 == 0 && num < 100 && num > 0) {
+    alert('O número atende aos critérios')
+}
+else {
+    alert('O número não atende aos critérios')
+}
